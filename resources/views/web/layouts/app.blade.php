7c70709e9f067d825374339bc21355ac48441cd4
TexusCode <texus.tj@gmail.com>
2024-11-11 04:22:34 +0500

    first commit

--- resources/views/web/layouts/app.blade.php
+++ resources/views/web/layouts/app.blade.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ config('app.name') }}</title>
+    <link rel="icon" href="{{ asset('assets/logo.png') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('assets/logo.png') }}" type="image/x-icon">
+    <title>{{$title ?? config('app.name') }}</title>
     @include('global.vite')
     @include('global.styles')
     @yield('styles')
